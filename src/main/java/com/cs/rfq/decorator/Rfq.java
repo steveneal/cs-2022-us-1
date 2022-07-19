919fbf1a009954e77230308c68a6c67def5adafb
Ethan Kenny <ethankenny47@yahoo.com>
2022-07-19 16:47:55 +0100

    completed rfq class and rfqtest passes

--- src/main/java/com/cs/rfq/decorator/Rfq.java
+++ src/main/java/com/cs/rfq/decorator/Rfq.java
@@ -12,14 +12,26 @@ public class Rfq implements Serializable {
     private String isin;
     private Long traderId;
     private Long entityId;
+    private Long instrumentId;
     private Long quantity;
     private Double price;
     private String side;
-    //this is a test
 
     public static Rfq fromJson(String json) {
-        //TODO: build a new RFQ setting all fields from data passed in the RFQ json message
-        return null;
+        Rfq newRfq = new Rfq();
+        Type t = new TypeToken<Map<String, String>>() {}.getType();
+        Map<String, String> fields = new Gson().fromJson(json, t);
+
+        newRfq.setId(fields.get("id"));
+        newRfq.setIsin(fields.get("isin"));
+        newRfq.setTraderId(Long.parseLong(fields.get("traderId")));
+        newRfq.setEntityId(Long.parseLong(fields.get("entityId")));
+        newRfq.setInstrumentId(Long.parseLong(fields.get("instrumentId")));
+        newRfq.setQuantity(Long.parseLong(fields.get("qty")));
+        newRfq.setPrice(Double.parseDouble(fields.get("price")));
+        newRfq.setSide(fields.get("side"));
+
+        return newRfq;
     }
 
     @Override
@@ -29,6 +41,7 @@ public class Rfq implements Serializable {
                 ", isin='" + isin + '\'' +
                 ", traderId=" + traderId +
                 ", entityId=" + entityId +
+                ", instrumentID=" + instrumentId +
                 ", quantity=" + quantity +
                 ", price=" + price +
                 ", side=" + side +
@@ -51,13 +64,9 @@ public class Rfq implements Serializable {
         this.id = id;
     }
 
-    public String getIsin() {
-        return isin;
-    }
+    public String getIsin() { return isin; }
 
-    public void setIsin(String isin) {
-        this.isin = isin;
-    }
+    public void setIsin(String isin) { this.isin = isin; }
 
     public Long getTraderId() {
         return traderId;
@@ -67,13 +76,13 @@ public class Rfq implements Serializable {
         this.traderId = traderId;
     }
 
-    public Long getEntityId() {
-        return entityId;
-    }
+    public Long getEntityId() { return entityId; }
 
-    public void setEntityId(Long entityId) {
-        this.entityId = entityId;
-    }
+    public void setEntityId(Long entityId) { this.entityId = entityId; }
+
+    public Long getInstrumentId() { return instrumentId; }
+
+    public void setInstrumentId(Long instrumentId) { this.instrumentId = instrumentId; }
 
     public Long getQuantity() {
         return quantity;
--- src/test/java/com/cs/rfq/decorator/RfqTest.java
+++ src/test/java/com/cs/rfq/decorator/RfqTest.java
@@ -10,9 +10,10 @@ public class RfqTest {
     public void testJSonFactoryMethod() {
         String validRfqJson = "{" +
                 "'id': '123ABC', " +
+                "'isin': 'AT0000383864', " +
                 "'traderId': 3351266293154445953, " +
                 "'entityId': 5561279226039690843, " +
-                "'instrumentId': 'AT0000383864', " +
+                "'instrumentId': 40972357234982251, " +
                 "'qty': 250000, " +
                 "'price': 1.58, " +
                 "'side': 'B' " +
@@ -22,9 +23,10 @@ public class RfqTest {
 
         assertAll(
                 () -> assertEquals("123ABC", rfq.getId()),
+                () -> assertEquals("AT0000383864", rfq.getIsin()),
                 () -> assertEquals((Long) 3351266293154445953L, rfq.getTraderId()),
                 () -> assertEquals((Long) 5561279226039690843L, rfq.getEntityId()),
-                () -> assertEquals("AT0000383864", rfq.getIsin()),
+                () -> assertEquals(40972357234982251L, rfq.getInstrumentId()),
                 () -> assertEquals((Long) 250000L, rfq.getQuantity()),
                 () -> assertEquals((Double) 1.58, rfq.getPrice()),
                 () -> assertEquals("B", rfq.getSide()),
